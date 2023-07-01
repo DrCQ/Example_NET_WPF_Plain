b6de684753750be0722bcc108db6529483811fd2
Christoph <dev@drcqsoft.com>
2023-07-01 13:23:07 +0200

    Add project files.

---
+++ Example_NET_WPF_Plain/MainWindowViewModel.vb
@@ -0,0 +1,17 @@
+﻿Imports CommunityToolkit.Mvvm.ComponentModel
+Imports CommunityToolkit.Mvvm.Input
+
+Namespace ViewModel
+
+    Public Class MainWindowViewModel
+        Inherits ObservableObject
+
+        ReadOnly Property CloseCommand As New RelayCommand(Of Window)(
+            Sub(wnd As Window)
+                wnd.Close()
+            End Sub)
+
+    End Class
+
+End Namespace
+
---
+++ Example_NET_WPF_Plain/ViewModelLocator.vb
@@ -0,0 +1,53 @@
+﻿Imports Microsoft.Extensions.DependencyInjection
+
+Namespace ViewModel
+
+    Public NotInheritable Class ViewModelLocator
+
+#Region "Private variables"
+
+        Private _mainVM As MainWindowViewModel = Nothing
+
+#End Region
+
+#Region "Properties: MainWindowVM"
+
+        ''' <summary>
+        ''' returns the <see cref="MainWindowViewModel"/> instance
+        ''' </summary>
+        ReadOnly Property MainWindowVM As MainWindowViewModel
+            Get
+                If Me._mainVM Is Nothing Then _mainVM = VMServices.GetService(Of MainWindowViewModel)
+                Return _mainVM
+            End Get
+        End Property
+
+#End Region
+
+#Region "Private properties: VMServices"
+
+        Private Shared Property VMServices As IServiceProvider = Nothing
+
+#End Region
+
+        Sub New()
+
+            If VMServices Is Nothing Then
+
+                'Create a new instance of ServiceCollection for 
+                Dim services As New ServiceCollection
+
+                'register all ViewModel classes
+                services.AddTransient(Of MainWindowViewModel)
+
+                'Assign the resulting service provide to the private VMServices Property
+                VMServices = services.BuildServiceProvider
+
+            End If
+
+        End Sub
+
+    End Class
+
+End Namespace
+
